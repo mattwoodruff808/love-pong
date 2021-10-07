ee0a2411a5cb9153565ddf00d939dfafc86fb568
mattwoodruff808 <mjwoodruff93@gmail.com>
2021-10-06 21:52:44 -0600

    Initial commit

---
+++ main.lua
@@ -0,0 +1,82 @@
+push = require 'push'
+
+WINDOW_WIDTH = 1280
+WINDOW_HEIGHT = 720
+
+VIRTUAL_WIDTH = 432
+VIRTUAL_HEIGHT = 243
+
+PADDLE_SPEED = 200
+
+function love.load()
+    -- OPTION 1 without PUSH:
+    -- love.window.setMode(WINDOW_WIDTH, WINDOW_HEIGHT, {
+    --     fullscreen = false,
+    --     resizable = false,
+    --     vsync = true
+    -- })
+
+    love.graphics.setDefaultFilter('nearest', 'nearest')
+
+    smallFont = love.graphics.newFont('font.ttf', 8)
+
+    scoreFont = love.graphics.newFont('font.ttf', 32)
+
+    love.graphics.setFont(smallFont)
+
+    -- With PUSH:
+    push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
+        fullscreen = false,
+        resizable = false,
+        vsync = true
+    })
+
+    player1Score = 0
+    player2Score = 0
+
+    player1Y = 30
+    player2Y = VIRTUAL_HEIGHT - 50
+end
+
+function love.update(dt)
+    if love.keyboard.isDown('w') then
+        player1Y = player1Y + -PADDLE_SPEED * dt
+    elseif love.keyboard.isDown('s') then
+        player1Y = player1Y + PADDLE_SPEED * dt
+    end
+
+    if love.keyboard.isDown('up') then
+        player2Y = player2Y + -PADDLE_SPEED * dt
+    elseif love.keyboard.isDown('down') then
+        player2Y = player2Y + PADDLE_SPEED * dt
+    end
+end
+
+function love.keypressed(key)
+    if key == 'escape' then
+        love.event.quit()
+    end
+end
+
+function love.draw()
+    push:apply('start')
+
+    love.graphics.clear(40/255, 45/255, 42/255, 255/255)
+
+    love.graphics.printf("Hello Pong!", 0, 20, VIRTUAL_WIDTH, "center")
+
+    love.graphics.setFont(scoreFont)
+    love.graphics.print(tostring(player1Score), VIRTUAL_WIDTH / 2 - 50, VIRTUAL_HEIGHT / 3)
+    love.graphics.print(tostring(player2Score), VIRTUAL_WIDTH / 2 + 30, VIRTUAL_HEIGHT / 3)
+
+    -- Left Paddle
+    love.graphics.rectangle('fill', 10, player1Y, 5, 20)
+
+    -- Right Paddle
+    love.graphics.rectangle('fill', VIRTUAL_WIDTH - 10, player2Y, 5, 20)
+
+    -- Ball
+    love.graphics.rectangle('fill', VIRTUAL_WIDTH / 2 - 2, VIRTUAL_HEIGHT / 2 - 2, 4, 4)
+
+    push:apply('end')
+end
